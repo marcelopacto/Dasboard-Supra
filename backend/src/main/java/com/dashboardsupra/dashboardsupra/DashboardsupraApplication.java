a0999b77977925ecd10c6a489671e02b7a138d83
marcelopacto <marcelo@pactosistemas.com.br>
2021-10-30 16:02:56 -0400

    Criacao do Projeto Dashboard Supra

---
+++ backend/src/main/java/com/dashboardsupra/dashboardsupra/DashboardsupraApplication.java
@@ -0,0 +1,13 @@
+package com.dashboardsupra.dashboardsupra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardsupraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardsupraApplication.class, args);
+	}
+
+}
